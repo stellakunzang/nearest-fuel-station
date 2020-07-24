087adf518b3a6d3823cad466436959f124dc2522
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-24 10:39:16 -0600

    Add mapquest to search result poro

--- app/poros/search_result.rb
+++ app/poros/search_result.rb
@@ -1,18 +1,20 @@
 class SearchResult
 
-  attr_reader :location
+  attr_reader :location, :station
 
   def initialize(location)
     @location = location
+    @station = station.address + ' ' + station.city + ', ' + station.state + ' ' + station.zip
   end
 
   def station
-    service = NRELService.new.nearest_station(@location)
-    Station.new(service)
-    binding.pry
+    station_data = NRELService.new.nearest_station(@location)
+    Station.new(station_data)
   end
 
   def directions
+    directions_data = MapQuestService.new.directions(@location, @station)
+    Directions.new(directions_data)
   end
 
 end
