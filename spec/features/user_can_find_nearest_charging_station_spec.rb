58b688a1f42e59dd0188b960e1fa358ce4e3ad0c
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-24 09:35:45 -0600

    Complete test

--- spec/features/user_can_find_nearest_charging_station_spec.rb
+++ spec/features/user_can_find_nearest_charging_station_spec.rb
@@ -10,5 +10,26 @@ describe "user can select Turing from drop down on search form" do
 
     expect(current_path).to eq('/search')
 
+    within(".name") do
+      expect(page).to have_content("Denver West Bldg 17")
+    end
+    within(".address") do
+      expect(page).to have_content("1626 Cole Boulevard")
+    end
+    within(".fuel") do
+      expect(page).to have_content("")
+    end
+    within(".times") do
+      expect(page).to have_content("")
+    end
+    within(".distance") do
+      expect(page).to have_content("0.1 miles")
+    end
+    within(".travel_time") do
+      expect(page).to have_content("1 minute")
+    end
+    within(".directions") do
+      expect(page).to have_content("Turn left onto Lawrence St Destination will be on the left")
+    end
   end
 end
