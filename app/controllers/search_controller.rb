b9dbab1c9b70bf235a29cb060db52d42595cb8b9
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-24 10:52:02 -0600

    Remove comments

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,6 @@ class SearchController < ApplicationController
   def index
     search = SearchResult.new(params[:location])
     @station = search.station
-#     @address="1225 17th St.",
-#     @city="Denver",
-#     @fuel="ELEC",
-#     @name="Seventeenth Street Plaza",
-#     @state="CO",
-#     @zip="80202"
     @directions = search.directions
-#     @directions="Start out going southeast on 17th St toward Larimer St/CO-33.1225 17TH ST is on the right.",
-#     @distance=0.1,
-#     @travel_time="00:00:40"
   end
 end
