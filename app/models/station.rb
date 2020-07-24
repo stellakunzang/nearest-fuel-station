f608ab69c026f9ae651284fe40a961b49cb52e86
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-24 10:38:41 -0600

    Add times to station model

--- app/models/station.rb
+++ app/models/station.rb
@@ -9,5 +9,6 @@ class Station
     @state = data[:fuel_stations][0][:state]
     @zip = data[:fuel_stations][0][:zip]
     @fuel = data[:fuel_stations][0][:fuel_type_code]
+    @times = data[:fuel_stations][0][:access_days_time]
   end
 end
