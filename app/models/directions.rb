07f753e3247df5ff94973af761c035298ff2df3b
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-24 10:39:39 -0600

    Create directions model

---
+++ app/models/directions.rb
@@ -0,0 +1,19 @@
+class Directions
+
+  attr_reader :distance, :travel_time, :directions
+
+  def initialize(data)
+    @data = data
+    @distance = @data[:route][:distance].round(1).to_s + " miles"
+    @travel_time = @data[:route][:formattedTime]
+    @directions = narrative(@data)
+  end
+
+  def narrative(data)
+    narrative = ""
+    data[:route][:legs][0][:maneuvers].map do |maneuver|
+      narrative += maneuver[:narrative]
+    end
+    narrative
+  end
+end
